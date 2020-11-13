61a6715c6f4daa9ae9c24cfdc79cb7341fc3db39
bhramar <bhramar115@gmail.com>
2020-11-13 19:22:33 +0530

    Add python implementation

---
+++ python/page-rank.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def pagerank(matrix, num_iterations, damping_factor):
+	dim = matrix.shape[1]
+	rank_vector = np.random.rand(dim,1)
+	rank_vector = rank_vector / np.linalg.norm(rank_vector,1)
+	matrix_hat = (damping_factor*matrix + (1 - damping_factor)/dim)
+
+	for i in range(num_iterations):
+		rank_vector = matrix_hat @ rank_vector
+
+	return rank_vector
+
+
+matrix = np.array([[0,0,0,0,1],[0.5,0,0,0,0],[0,1,0.5,0,0],[0,0,0.5,1,0],[0.5,0,0,0,0]])
+
+v = pagerank(matrix, 100, 0.85)
+print(v)
